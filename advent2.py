5c37e94f4430b34e69c957e30aa91e9273ae2134
ald <aldoti@gmail.com>
2020-12-06 21:51:09 -0500

    updated a2 part1

--- advent2.py
+++ advent2.py
@@ -1,25 +1,25 @@
-def valid_pws(passwords):
+def part1():
+	a = ""
+	b = ""
+	c = ""
+	pw = []
+	with open('advent2.txt') as file:
+		lines = file.read().splitlines()
+		for line in lines:
+			temp = {"min":0,"max":0,"char":"","pw":""}
+			a,b,c = line.split(" ")
+			temp["min"], temp["max"] = map(int,a.split("-"))
+			temp["char"] = b[0]
+			temp["pw"] = c
+			pw.append(temp)
 	counter = 0
-	for line in passwords:
-		print(line["pw"] + " " + line["char"] + " " + str(range(line["min"],line["max"])))
+	for line in pw:
+		#print(line["pw"] + " " + line["char"] + " " + str(range(line["min"],line["max"])))
 		if line["pw"].count(line["char"]) in range(line["min"],line["max"]+1):
 			counter += 1
-			print("yes")
+		#	print("yes")
 	return counter
 
 
-a = ""
-b = ""
-c = ""
-pw = []
-with open('advent2.txt') as file:
-	lines = file.read().splitlines()
-	for line in lines:
-		temp = {"min":0,"max":0,"char":"","pw":""}
-		a,b,c = line.split(" ")
-		temp["min"], temp["max"] = map(int,a.split("-"))
-		temp["char"] = b[0]
-		temp["pw"] = c
-		pw.append(temp)
 
-print(valid_pws(pw))
+print(part1())
